3383a02a22b1b1acd91c6fce0efdb2dbb237e1f0
re1n-e <raghavendrabargali@gmail.com>
2025-04-16 20:55:44 +0530

    commit

--- compiler.go
+++ compiler.go
@@ -1,10 +1,26 @@
 package main
 
+import "fmt"
+
 func Compile(source string) {
 	var scanner Scanner
 	scanner.InitScanner(source)
 	line := -1
 	for {
 		token := scanner.scanToken()
+		if token.line != line {
+			fmt.Printf("%4d ", token.line)
+			line = token.line
+		} else {
+			fmt.Printf("   | ")
+		}
+
+		// Properly slice and convert to string
+		lexeme := string(token.start)
+		fmt.Printf("%2d '%s'\n", token.typ_e, lexeme)
+
+		if token.typ_e == TOKEN_EOF {
+			break
+		}
 	}
 }
--- scanner.go
+++ scanner.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"unicode"
+)
+
 type TokenType int
 
 // Single-character tokens.
@@ -71,19 +76,107 @@ type Token struct {
 }
 
 func (scanner *Scanner) scanToken() Token {
+	scanner.skipWhitespace()
 	scanner.Start = scanner.Current
 
 	if scanner.isAtEnd() {
 		return scanner.makeToken(TOKEN_EOF)
 	}
 
-	return scanner.errorToken("Unexpected character.")
+	c := scanner.advance()
+	if unicode.IsLetter(c) {
+		return scanner.identifier()
+	}
+	if unicode.IsDigit(c) {
+		return scanner.number()
+	}
+
+	switch c {
+	case '(':
+		return scanner.makeToken(TOKEN_LEFT_PAREN)
+	case ')':
+		return scanner.makeToken(TOKEN_RIGHT_PAREN)
+	case '{':
+		return scanner.makeToken(TOKEN_LEFT_BRACE)
+	case '}':
+		return scanner.makeToken(TOKEN_RIGHT_BRACE)
+	case ';':
+		return scanner.makeToken(TOKEN_SEMICOLON)
+	case ',':
+		return scanner.makeToken(TOKEN_COMMA)
+	case '.':
+		return scanner.makeToken(TOKEN_DOT)
+	case '-':
+		return scanner.makeToken(TOKEN_MINUS)
+	case '+':
+		return scanner.makeToken(TOKEN_PLUS)
+	case '/':
+		return scanner.makeToken(TOKEN_SLASH)
+	case '*':
+		return scanner.makeToken(TOKEN_STAR)
+	case '!':
+		if scanner.match('=') {
+			return scanner.makeToken(TOKEN_BANG_EQUAL)
+		}
+		return scanner.makeToken(TOKEN_BANG)
+	case '=':
+		if scanner.match('=') {
+			return scanner.makeToken(TOKEN_EQUAL_EQUAL)
+		}
+		return scanner.makeToken(TOKEN_EQUAL)
+	case '<':
+		if scanner.match('=') {
+			return scanner.makeToken(TOKEN_LESS_EQUAL)
+		}
+		return scanner.makeToken(TOKEN_LESS)
+	case '>':
+		if scanner.match('=') {
+			return scanner.makeToken(TOKEN_GREATER_EQUAL)
+		}
+		return scanner.makeToken(TOKEN_GREATER)
+	case '"':
+		return scanner.string()
+	}
+	return scanner.errorToken(fmt.Sprintf("Unexpected character. %c", c))
+}
+
+func (scanner *Scanner) advance() rune {
+	if scanner.isAtEnd() {
+		return 0
+	}
+	scanner.Current++
+	return scanner.Source[scanner.Current-1]
+}
+
+func (scanner *Scanner) peek() rune {
+	if scanner.isAtEnd() {
+		return 0
+	}
+	return scanner.Source[scanner.Current]
+}
+
+func (scanner *Scanner) peekNext() rune {
+	if scanner.isAtEnd() {
+		return ' '
+	}
+	return scanner.Source[scanner.Current+1]
+}
+
+func (scanner *Scanner) match(expected rune) bool {
+	if scanner.isAtEnd() {
+		return false
+	}
+	if scanner.Source[scanner.Current] != expected {
+		return false
+	}
+	scanner.Current++
+	return true
 }
 
 func (scanner *Scanner) makeToken(typ_e TokenType) Token {
 	var token Token
 	token.typ_e = typ_e
-	token.start = scanner.Source
+	token.start = scanner.Source[scanner.Start:scanner.Current]
 	token.length = scanner.Current - scanner.Start
 	token.line = scanner.Line
 	return token
@@ -96,6 +189,124 @@ func (scanner *Scanner) errorToken(message string) Token {
 	token.length = len(message)
 	token.line = scanner.Line
 	return token
+}
+
+func (scanner *Scanner) skipWhitespace() {
+	for !scanner.isAtEnd() {
+		switch scanner.peek() {
+		case ' ', '\r', '\t', '\n':
+			if scanner.peek() == '\n' {
+				scanner.Line++
+			}
+			scanner.advance()
+		case '/':
+			if scanner.peekNext() == '/' {
+				for scanner.peek() != '\n' && !scanner.isAtEnd() {
+					scanner.advance()
+				}
+			} else {
+				return
+			}
+		default:
+			return
+		}
+	}
+}
+func (scanner *Scanner) checkKeyword(start int, length int, rest string, typ_e TokenType) TokenType {
+	if scanner.Current-scanner.Start == start+length {
+		slice := string(scanner.Source[scanner.Start+start : scanner.Start+start+length])
+		if slice == rest {
+			return typ_e
+		}
+	}
+	return TOKEN_IDENTIFIER
+}
+
+func (scanner *Scanner) identifier() Token {
+	for unicode.IsLetter(scanner.peek()) || unicode.IsDigit(scanner.peek()) {
+		scanner.advance()
+	}
+	return scanner.makeToken(scanner.identifierType())
+}
+
+func (scanner *Scanner) identifierType() TokenType {
+	switch scanner.Source[scanner.Start] {
+	case 'a':
+		return scanner.checkKeyword(1, 2, "nd", TOKEN_AND)
+	case 'c':
+		return scanner.checkKeyword(1, 4, "lass", TOKEN_CLASS)
+	case 'e':
+		return scanner.checkKeyword(1, 3, "lse", TOKEN_ELSE)
+	case 'f':
+		if scanner.Current-scanner.Start > 1 {
+			switch scanner.Source[scanner.Start+1] {
+			case 'a':
+				return scanner.checkKeyword(1, 4, "alse", TOKEN_FALSE)
+			case 'o':
+				return scanner.checkKeyword(1, 2, "or", TOKEN_FOR)
+			case 'u':
+				return scanner.checkKeyword(1, 2, "un", TOKEN_FUN)
+			}
+		}
+	case 'i':
+		return scanner.checkKeyword(1, 1, "f", TOKEN_IF)
+	case 'n':
+		return scanner.checkKeyword(1, 2, "il", TOKEN_NIL)
+	case 'o':
+		return scanner.checkKeyword(1, 1, "r", TOKEN_OR)
+	case 'p':
+		return scanner.checkKeyword(1, 4, "rint", TOKEN_PRINT)
+	case 'r':
+		return scanner.checkKeyword(1, 5, "eturn", TOKEN_RETURN)
+	case 's':
+		return scanner.checkKeyword(1, 4, "uper", TOKEN_SUPER)
+	case 't':
+		if scanner.Current-scanner.Start > 1 {
+			switch scanner.Source[scanner.Start+1] {
+			case 'h':
+				return scanner.checkKeyword(1, 3, "his", TOKEN_THIS)
+			case 'r':
+				return scanner.checkKeyword(1, 3, "rue", TOKEN_TRUE)
+			}
+		}
+	case 'v':
+		return scanner.checkKeyword(1, 2, "ar", TOKEN_VAR)
+	case 'w':
+		return scanner.checkKeyword(1, 4, "hile", TOKEN_WHILE)
+	}
+	return TOKEN_IDENTIFIER
+}
+
+func (scanner *Scanner) number() Token {
+	for unicode.IsDigit(scanner.peek()) {
+		scanner.advance()
+	}
+
+	if scanner.peek() == '.' && unicode.IsDigit(scanner.peekNext()) {
+		scanner.advance()
+
+		for unicode.IsDigit(scanner.peek()) {
+			scanner.advance()
+		}
+	}
+
+	return scanner.makeToken(TOKEN_NUMBER)
+}
+
+func (scanner *Scanner) string() Token {
+	for scanner.peek() != '"' && !scanner.isAtEnd() {
+		if scanner.peek() == '\n' {
+			scanner.Line++
+		}
+		scanner.advance()
+	}
+
+	if scanner.isAtEnd() {
+		return scanner.errorToken("Unterminated string.")
+	}
+
+	scanner.advance()
+	return scanner.makeToken(TOKEN_STRING)
 }
 
 func (scanner *Scanner) isAtEnd() bool {
